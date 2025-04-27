0c9bab85a46f9ab1596a1f9af86f0de24b7fb557
Luis Sandoval <ljulian199914@gmail.com>
2025-04-27 11:46:15 -0500

    Se optimizo la obtencion del id del monto maximo y el verificador de tipos de transacciones

--- analysis.py
+++ analysis.py
@@ -10,22 +10,24 @@ tipos = ['Crédito', 'Débito']
 if set(df['tipo']) == set(tipos):
     pass
 else:
-    print("Error: El archivo contiene más de dos tipos de transacciones") 
+    raise ValueError("El archivo contiene más tipos de transacciones de los esperados.")
 
-
-#Agrupar por tipo  y sumar los montos de tipo Crédito menos la suma de los montos de tipo Débito
+#Agrupo por tipo  y sumo los montos de tipo Crédito menos la suma de los montos de tipo Débito
 sum = df.groupby('tipo')['monto'].sum()
 balance_final = (sum['Crédito'] - sum['Débito'])
 
-# ID y el monto de la transacción con el valor más alto.
-max_monto = df.max()
-id_max = (df['monto'].idxmax() + 1 )
+# Obtengo el monto máximo
+max_monto = df['monto'].max()
+
+# Obtengo el id de la transacción con el monto máximo
+id_max = df.loc[df['monto'].idxmax(), 'id' ]
 
 # Agrupar por tipo y contar el numero de transacciones por tipo
 count = df.groupby('tipo')['monto'].count()
 
+# Imprimir el reporte
 print(f'''Reporte de Transacciones 
 ----------------------------------------------------------
 Balance Final:{balance_final:.2f}
-Transacción de Mayor Monto: id: {id_max} - {max_monto.monto}
+Transacción de Mayor Monto: id: {id_max} - {max_monto}
 Conteo de Transacciones: Crédito:{count['Crédito']}  Débito:{count['Débito']} ''')
