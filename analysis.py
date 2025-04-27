587cb25b408c1e5dcdacf95386d046fde122387f
Luis Sandoval <ljulian199914@gmail.com>
2025-04-27 11:11:15 -0500

    Se coloco la verificacion de tipos de transacciones

--- analysis.py
+++ analysis.py
@@ -1,18 +1,21 @@
 #Importar dependecias 
 import pandas as pd
 
-# Leer el archivo csv
+# Cargar el archivo CSV 
 df = pd.read_csv("data.csv")
 
-# Verificar el numero de filas y columnas
-print(df.shape)
+#Verificamos que no haya error de tipeo en los tipos de transacciones
+tipos = ['Crédito', 'Débito']
+
+if set(df['tipo']) == set(tipos):
+    pass
+else:
+    print("Error: El archivo contiene más de dos tipos de transacciones") 
+
 
 #Agrupar por tipo  y sumar los montos de tipo Crédito menos la suma de los montos de tipo Débito
 sum = df.groupby('tipo')['monto'].sum()
 balance_final = (sum['Crédito'] - sum['Débito'])
-#print(sum)
-print(type(sum))
-#print(balance_final)
 
 # ID y el monto de la transacción con el valor más alto.
 max_monto = df.max()
@@ -21,10 +24,8 @@ id_max = (df['monto'].idxmax() + 1 )
 # Agrupar por tipo y contar el numero de transacciones por tipo
 count = df.groupby('tipo')['monto'].count()
 
-print(count['Crédito'])
-
 print(f'''Reporte de Transacciones 
-----------------------------------------------
+----------------------------------------------------------
 Balance Final:{balance_final:.2f}
 Transacción de Mayor Monto: id: {id_max} - {max_monto.monto}
 Conteo de Transacciones: Crédito:{count['Crédito']}  Débito:{count['Débito']} ''')
